088c4aac1655880ceb0794808b96194c0f8d832a
carrotluc <lucicarcam@gmail.com>
2026-04-24 13:10:19 +0200

    caption controller & repository done and tested (it all worked)

---
+++ src/main/java/aiss/DailyMotionMiner/controllerDM/CaptionDMController.java
@@ -0,0 +1,31 @@
+package aiss.DailyMotionMiner.controllerDM;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import aiss.DailyMotionMiner.modelDM.caption.CaptionList;
+import aiss.DailyMotionMiner.repositoryDM.CaptionDMRepository;
+
+@RestController
+@RequestMapping("DailyMotionMiner/captions")
+public class CaptionDMController {
+
+    private CaptionDMRepository captionDMRepository;
+
+    @Autowired
+    public CaptionDMController (CaptionDMRepository captionDMRepository) {
+        this.captionDMRepository = captionDMRepository;
+    }
+
+    // GET http://localhost:8081/DailyMotionMiner/captions/{id}
+    @GetMapping("/{idVideo}")
+    public List<CaptionList> getCaptions(@PathVariable String idVideo) {
+        return captionDMRepository.findAll(idVideo);
+    }
+
+}
---
+++ src/main/java/aiss/DailyMotionMiner/repositoryDM/CaptionDMRepository.java
@@ -0,0 +1,19 @@
+package aiss.DailyMotionMiner.repositoryDM;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import aiss.DailyMotionMiner.modelDM.caption.CaptionList;
+import aiss.DailyMotionMiner.services.CaptionDMService;
+
+@Repository
+public class CaptionDMRepository {
+    @Autowired
+    CaptionDMService captionDMService;
+
+    public List<CaptionList> findAll(String idVideo) {
+        List<CaptionList> captions = captionDMService.getCaptions(idVideo);
+        return captions;
+    }
+}
